92a35ace42bec3c2008b53f81d5d5aa87f7ede6e
Ido Shemi <shemiido@gmail.com>
2022-10-15 11:15:44 +0300

    added database and insertpage

---
+++ WebProject/WebProject/InsertUser.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InsertUser.aspx.cs" Inherits="WebProject.InsertUser" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:TextBox ID="InsertName" runat="server" Text="הכנס שם משתמש"></asp:TextBox><br />
+            <asp:TextBox ID="InsertPass" runat="server" Text="הכנס סיסמא"></asp:TextBox><br />
+            <asp:TextBox ID="InsertPhone" runat="server" Text="הכנס מספר טלפון"></asp:TextBox><br />
+            <asp:TextBox ID="InsertMail" runat="server" Text="הכנס כתובת מייל"></asp:TextBox><br />
+
+
+            <div class="selectGender">
+                מין:
+                <asp:RadioButton ID="RadioButton1" Text="male" runat="server" GroupName="gender" />
+                <asp:RadioButton ID="RadioButton2" Text="female" runat="server" GroupName="gender" />
+                <br />
+            </div>
+
+
+            תאריך לידה:
+            <asp:TextBox ID="birthDay" runat="server" Width="55px" Text="day"></asp:TextBox>
+            <asp:TextBox ID="birthMonth" runat="server" Width="47px" Text="month"> </asp:TextBox>
+            <asp:TextBox ID="birthYear" runat="server" Width="89px" Text="year"></asp:TextBox>
+            <br />
+            <asp:Button runat="server" Text="לחץ להוספה" OnClick="Insertbutton" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebProject/WebProject/InsertUser.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace WebProject
+{
+    public partial class InsertUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Insertbutton(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection Con1 = new OleDbConnection();
+                //  OleDbCommand cmd = null;
+                Con1.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data source=" + Server.MapPath("") + "\\database.accdb";
+
+                string gender = "";
+                if (RadioButton1.Checked) gender = "male";
+                else gender = "female";
+
+                string sqlstring = " INSERT INTO users (myusername,mypassword, mybirthdate, myemail, mygender, myphonenumber) VALUES "
+                                + "('" + InsertName.Text + "','" + InsertPass.Text + "','" + birthDay.Text+'/'+ birthMonth.Text+'/'+ birthYear.Text + "','" +
+                                InsertMail.Text + "','" + gender + "','" + InsertPhone.Text + "');";
+
+                //string sqlstring = "INSERT INTO users(UserName, Password,PhoneNum) values ('" + InsertText.Text + "','1234')";
+                Con1.Open();
+                OleDbCommand cmd = new OleDbCommand(sqlstring, Con1);
+                int y = 0;
+                y = cmd.ExecuteNonQuery();
+                Response.Write(y);
+                Con1.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+    }
+}
